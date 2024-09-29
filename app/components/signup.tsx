4a93492a004c04603e8a97f73b35792264174a9c
akinloluwami <akinkunmioye42@gmail.com>
2024-09-29 04:02:44 +0100

    Create signup component

---
+++ app/components/signup.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div className="w-[400px] bg-black rounded-xl px-5 py-10 flex flex-col">
+      <h2 className="text-center text-3xl font-bold">Dropp.</h2>
+      <p className="text-center text-sm">Create an account</p>
+      <input
+        type="email"
+        placeholder="Email"
+        className="bg-[#242424]/40 p-3 rounded-xl mt-7 outline-none"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className="bg-[#242424]/40 p-3 rounded-xl mt-5 outline-none"
+      />
+      <input
+        type="password"
+        placeholder="Confirm Password"
+        className="bg-[#242424]/40 p-3 rounded-xl mt-5 outline-none"
+      />
+      <button className="bg-white text-black rounded-xl font-semibold p-3 mt-5">
+        Signup
+      </button>
+    </div>
+  );
+};
+
+export default Signup;
