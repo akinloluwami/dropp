d344cb0f0d20591fced986ccde9f601c077e51c4
akinloluwami <akinkunmioye42@gmail.com>
2024-09-30 04:54:59 +0100

    signup

--- pages/components/signup.tsx
+++ pages/components/signup.tsx
@@ -1,60 +1,133 @@
-import React from "react";
-// import { useFormState, useFormStatus } from "react-dom";
-// import { signup } from "../actions/auth/signup";
+import React, { FC, useState } from "react";
+import axios from "axios";
+import { SignupFormSchema, SignupFormState } from "../lib/definitions";
+import { LoaderCircle } from "lucide-react";
 
-const Signup = () => {
-  // const [state, action] = useFormState(signup, undefined);
-  // const { pending } = useFormStatus();
+const fields = [
+  {
+    name: "name",
+    type: "text",
+    placeholder: "Name",
+    errorKey: "name",
+  },
+  {
+    name: "email",
+    type: "email",
+    placeholder: "Email",
+    errorKey: "email",
+  },
+  {
+    name: "password",
+    type: "password",
+    placeholder: "Password",
+    errorKey: "password",
+  },
+  {
+    name: "confirmPassword",
+    type: "password",
+    placeholder: "Confirm Password",
+    errorKey: "confirmPassword",
+  },
+];
+
+const Signup: FC<{
+  onSignupSuccess: () => void;
+}> = ({ onSignupSuccess }) => {
+  const [state, setState] = useState<SignupFormState>(undefined);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setState(undefined);
+
+    const result = SignupFormSchema.safeParse(formData);
+
+    if (!result.success) {
+      const fieldErrors = result.error.formErrors.fieldErrors;
+      setState({
+        errors: {
+          name: fieldErrors.name || [],
+          email: fieldErrors.email || [],
+          password: fieldErrors.password || [],
+          confirmPassword: fieldErrors.confirmPassword || [],
+        },
+      });
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      await axios.post("/api/auth/signup", formData);
+      onSignupSuccess();
+    } catch (error: any) {
+      setState({
+        message:
+          error.response.data.error || "Failed to sign up. Please try again.",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <form
       className="w-full max-w-[400px] mx-auto bg-black rounded-xl px-5 py-10 flex flex-col"
-      // action={action}
+      onSubmit={handleSubmit}
       onClick={(e) => e.stopPropagation()}
     >
       <h2 className="text-center text-3xl font-bold">Dropp.</h2>
       <p className="text-center text-sm">Create an account</p>
-      <input
-        type="text"
-        placeholder="Name"
-        name="name"
-        className="bg-[#242424]/40 p-3 rounded-xl mt-7 outline-none"
-      />
-      {/* {state?.errors?.name && (
-        <p className="text-sm text-red-500">{state.errors.name}</p>
-      )} */}
-      <input
-        type="email"
-        placeholder="Email"
-        name="email"
-        className="bg-[#242424]/40 p-3 rounded-xl mt-5 outline-none"
-      />
-      {/* {state?.errors?.email && (
-        <p className="text-sm text-red-500">{state.errors.email}</p>
-      )} */}
-      <input
-        type="password"
-        placeholder="Password"
-        name="password"
-        className="bg-[#242424]/40 p-3 rounded-xl mt-5 outline-none"
-      />
-      {/* {state?.errors?.password && (
-        <p className="text-sm text-red-500">{state.errors.password}</p>
-      )} */}
-      <input
-        type="password"
-        placeholder="Confirm Password"
-        name="confirmPassword"
-        className="bg-[#242424]/40 p-3 rounded-xl mt-5 outline-none"
-      />
-      {/* {state?.errors?.confirmPassword && (
-        <p className="text-sm text-red-500">{state.errors.confirmPassword}</p>
-      )} */}
+
+      {fields.map((field) => (
+        <div key={field.name}>
+          <input
+            type={field.type}
+            name={field.name}
+            placeholder={field.placeholder}
+            value={formData[field.name as keyof typeof formData]}
+            onChange={handleInputChange}
+            className="bg-[#242424]/40 p-3 rounded-xl mt-5 outline-none w-full"
+          />
+
+          {
+            //@ts-ignore
+            state?.errors?.[field.errorKey] && (
+              <p className="text-sm text-red-500">
+                {
+                  //@ts-ignore
+                  state.errors[field.errorKey]?.[0]
+                }
+              </p>
+            )
+          }
+        </div>
+      ))}
+
+      {state?.message && (
+        <p className="text-sm text-red-500">{state.message}</p>
+      )}
+
       <button
-        className="bg-white text-black rounded-xl font-semibold p-3 mt-5"
-        // disabled={pending}
+        className="bg-white text-black rounded-xl font-semibold p-3 mt-5 disabled:opacity-50 flex items-center justify-center disabled:cursor-not-allowed"
+        type="submit"
+        disabled={loading}
       >
-        Sign Up
+        {loading ? <LoaderCircle className="animate-spin" /> : "Sign Up"}
       </button>
     </form>
   );
