7ca1580fefe777ba271b9a23218a0f3e4621560f
akinloluwami <akinkunmioye42@gmail.com>
2024-09-30 23:33:00 +0100

    file functions

---
+++ utils/files.ts
@@ -0,0 +1,16 @@
+export const readableSize = (size: number) => {
+  const units = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  let i = 0;
+  while (size >= 1024) {
+    size /= 1024;
+    i++;
+  }
+  return `${size?.toFixed(2)} ${units[i]}`;
+};
+
+export const truncateFromEnd = (str: string, length: number) => {
+  if (str.length > length) {
+    return `...${str.substring(str.length - length)}`;
+  }
+  return str;
+};
