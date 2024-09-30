49b8c5ac139cb9cbfc8f5e2ed52cdaf366480825
akinloluwami <akinkunmioye42@gmail.com>
2024-09-30 04:54:10 +0100

    complete login endpoint

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -3,6 +3,8 @@ import { users } from "@/database/schema";
 import { eq } from "drizzle-orm";
 import { NextApiRequest, NextApiResponse } from "next/types";
 import { compare } from "bcryptjs";
+import { withErrorHandling } from "@/middlewares/error-handling";
+import { setTokenCookie } from "@/helpers/auth/setTokenCookie";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { email, password } = req.body;
@@ -10,16 +12,24 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const [user] = await db.select().from(users).where(eq(users.email, email));
 
   if (!user) {
-    return {
-      message: "Invalid email or password",
-    };
+    return res.status(401).json({
+      error: "Invalid email or password",
+    });
   }
 
   const isPasswordValid = await compare(password, user.password!);
 
   if (!isPasswordValid) {
-    return {
-      message: "Invalid email or password",
-    };
+    return res.status(401).json({
+      error: "Invalid email or password",
+    });
   }
+
+  setTokenCookie(req, res, user.id!);
+
+  return res.status(200).json({
+    message: "Logged in successfully",
+  });
 };
+
+export default withErrorHandling(handler, "POST");
