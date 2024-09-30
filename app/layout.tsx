1b2af2191fb7bb05c611a6866a58aa530df34873
akinloluwami <akinkunmioye42@gmail.com>
2024-09-30 02:50:00 +0100

    clean up layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,7 @@
 "use client";
 
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { Tooltip } from "react-tooltip";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function RootLayout({
   children,
@@ -23,9 +10,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <Tooltip id="tt" className="z-10" />
         {children}
       </body>
