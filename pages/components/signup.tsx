d25a01f8885383485cb0a3034271bbc3a2827055
akinloluwami <akinkunmioye42@gmail.com>
2024-09-30 21:50:00 +0100

    Update styling

--- pages/components/signup.tsx
+++ pages/components/signup.tsx
@@ -86,7 +86,7 @@ const Signup: FC<{
 
   return (
     <form
-      className="w-full max-w-[400px] mx-auto bg-black rounded-xl px-5 py-10 flex flex-col"
+      className="w-full max-w-[400px] mx-auto bg-black-001 rounded-xl px-5 py-10 flex flex-col"
       onSubmit={handleSubmit}
       onClick={(e) => e.stopPropagation()}
     >
