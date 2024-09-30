902037caa16629c1b79c60ecab0f8bc5cca3ce29
akinloluwami <akinkunmioye42@gmail.com>
2024-09-30 07:06:10 +0100

    Switched to TanStack Router

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Tooltip } from "react-tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Tooltip id="tt" className="z-10" />
       <Component {...pageProps} />
-    </>
+    </QueryClientProvider>
   );
 }
