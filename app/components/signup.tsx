2aa51a1c1a05af5066e477422d22a3eba0103f35
akinloluwami <akinkunmioye42@gmail.com>
2024-09-30 02:51:04 +0100

    Responsive

--- app/components/signup.tsx
+++ app/components/signup.tsx
@@ -1,29 +1,58 @@
 import React from "react";
+import { useFormState } from "react-dom";
+import { signup } from "../actions/auth/signup";
 
 const Signup = () => {
+  const [state, action] = useFormState(signup, undefined);
+
   return (
-    <div className="w-[400px] bg-black rounded-xl px-5 py-10 flex flex-col">
+    <form
+      className="w-full max-w-[400px] mx-auto bg-black rounded-xl px-5 py-10 flex flex-col"
+      action={action}
+      onClick={(e) => e.stopPropagation()}
+    >
       <h2 className="text-center text-3xl font-bold">Dropp.</h2>
       <p className="text-center text-sm">Create an account</p>
       <input
-        type="email"
-        placeholder="Email"
+        type="text"
+        placeholder="Name"
+        name="name"
         className="bg-[#242424]/40 p-3 rounded-xl mt-7 outline-none"
       />
+      {state?.errors?.name && (
+        <p className="text-sm text-red-500">{state.errors.name}</p>
+      )}
+      <input
+        type="email"
+        placeholder="Email"
+        name="email"
+        className="bg-[#242424]/40 p-3 rounded-xl mt-5 outline-none"
+      />
+      {state?.errors?.email && (
+        <p className="text-sm text-red-500">{state.errors.email}</p>
+      )}
       <input
         type="password"
         placeholder="Password"
+        name="password"
         className="bg-[#242424]/40 p-3 rounded-xl mt-5 outline-none"
       />
+      {state?.errors?.password && (
+        <p className="text-sm text-red-500">{state.errors.password}</p>
+      )}
       <input
         type="password"
         placeholder="Confirm Password"
+        name="confirmPassword"
         className="bg-[#242424]/40 p-3 rounded-xl mt-5 outline-none"
       />
+      {state?.errors?.confirmPassword && (
+        <p className="text-sm text-red-500">{state.errors.confirmPassword}</p>
+      )}
       <button className="bg-white text-black rounded-xl font-semibold p-3 mt-5">
         Signup
       </button>
-    </div>
+    </form>
   );
 };
 
