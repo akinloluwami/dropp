95baf7ca6fc1b2d5e48c7e6bcb0a316f9a5b3f76
akinloluwami <akinkunmioye42@gmail.com>
2024-09-30 02:49:33 +0100

    def

---
+++ app/lib/definitions.ts
@@ -0,0 +1,49 @@
+import { db } from "@/database";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+
+export const SignupFormSchema = z
+  .object({
+    name: z
+      .string()
+      .min(2, { message: "Name must be at least 2 characters long." })
+      .trim(),
+    email: z.string().email({ message: "Please enter a valid email." }).trim(),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters long." })
+      .trim(),
+    confirmPassword: z.string().trim(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords must match.",
+  });
+
+export type FormState =
+  | {
+      errors?: {
+        name?: string[];
+        email?: string[];
+        password?: string[];
+        confirmPassword?: string[];
+      };
+      message?: string;
+    }
+  | undefined;
+
+export const LoginFormSchema = z.object({
+  email: z.string().email({ message: "Please enter a valid email." }).trim(),
+  password: z.string({ message: "Password is required" }).trim(),
+});
+
+export type LoginFormState =
+  | {
+      errors?: {
+        email?: string[];
+        password?: string[];
+      };
+      message?: string;
+    }
+  | undefined;
