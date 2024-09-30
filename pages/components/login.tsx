a1021852c4b88feaccdfa165400caba916036e8e
akinloluwami <akinkunmioye42@gmail.com>
2024-09-30 04:54:50 +0100

    login component

--- pages/components/login.tsx
+++ pages/components/login.tsx
@@ -1,42 +1,97 @@
-import React from "react";
-// import { useFormState, useFormStatus } from "react-dom";
-// import { login } from "../actions/auth/login";
+import React, { FC, useState } from "react";
+import axios from "axios";
+import { LoginFormSchema, LoginFormState } from "../lib/definitions";
+import { LoaderCircle } from "lucide-react";
 
-const Login = () => {
-  // const [state, action] = useActionState(login, undefined);
-  // const { pending } = useFormStatus();
+const Login: FC<{
+  onLoginSuccess: () => void;
+}> = ({ onLoginSuccess }) => {
+  const [state, setState] = useState<LoginFormState>(undefined);
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setState(undefined);
+
+    const result = LoginFormSchema.safeParse(formData);
+
+    if (!result.success) {
+      const fieldErrors = result.error.formErrors.fieldErrors;
+      setState({
+        errors: {
+          email: fieldErrors.email || [],
+          password: fieldErrors.password || [],
+        },
+      });
+      return;
+    }
+
+    setLoading(true);
+    try {
+      await axios.post("/api/auth/login", formData);
+      onLoginSuccess();
+    } catch (error: any) {
+      setState({
+        message:
+          error.response.data.error || "Failed to login. Please try again.",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <form
       className="w-full max-w-[400px] mx-auto bg-black rounded-xl px-5 py-10 flex flex-col"
-      // action={action}
+      onSubmit={handleSubmit}
       onClick={(e) => e.stopPropagation()}
     >
       <h2 className="text-center text-3xl font-bold">Dropp.</h2>
       <p className="text-center text-sm">Log into your account</p>
+
       <input
         type="email"
-        placeholder="Email"
         name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleInputChange}
         className="bg-[#242424]/40 p-3 rounded-xl mt-7 outline-none"
       />
-      {/* {state?.errors?.email && (
-        <p className="text-sm text-red-500">{state.errors.email}</p>
-      )} */}
+      {state?.errors?.email && (
+        <p className="text-sm text-red-500 mt-1">{state.errors.email[0]}</p>
+      )}
+
       <input
         type="password"
         name="password"
         placeholder="Password"
+        value={formData.password}
+        onChange={handleInputChange}
         className="bg-[#242424]/40 p-3 rounded-xl mt-5 outline-none"
       />
-      {/* {state?.errors?.password && (
-        <p className="text-sm text-red-500">{state.errors.password}</p>
-      )} */}
+      {state?.errors?.password && (
+        <p className="text-sm text-red-500 mt-1">{state.errors.password[0]}</p>
+      )}
+
+      {state?.message && (
+        <p className="text-sm text-red-500">{state.message}</p>
+      )}
+
       <button
-        className="bg-white text-black rounded-xl font-semibold p-3 mt-5"
-        // disabled={pending}
+        className="bg-white text-black rounded-xl font-semibold p-3 mt-5 disabled:opacity-50 flex items-center justify-center disabled:cursor-not-allowed"
         type="submit"
+        disabled={loading}
       >
-        Login
+        {loading ? <LoaderCircle className="animate-spin" /> : "Login"}
       </button>
     </form>
   );
