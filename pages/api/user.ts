fb96dfdfe3d0240e9ce9e1f0920c06cb1165c5de
akinloluwami <akinkunmioye42@gmail.com>
2024-09-30 04:53:44 +0100

    get user info

---
+++ pages/api/user.ts
@@ -0,0 +1,23 @@
+import { db } from "@/database";
+import { users } from "@/database/schema";
+import { withAuth } from "@/middlewares/auth";
+import { withErrorHandling } from "@/middlewares/error-handling";
+import { eq } from "drizzle-orm";
+import { NextApiRequest, NextApiResponse } from "next/types";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const [user] = await db.select().from(users).where(eq(users.id, req.userId!));
+
+  if (!user) {
+    return res.status(404).json({
+      error: "User not found",
+    });
+  }
+
+  return res.status(200).json({
+    email: user.email,
+    name: user.name,
+  });
+};
+
+export default withErrorHandling(withAuth(handler), "GET");
