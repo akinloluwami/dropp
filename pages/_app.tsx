fd59bd5d6fd8b75e5411a161d8a17b4d67848e15
akinloluwami <akinkunmioye42@gmail.com>
2024-09-30 12:32:54 +0100

    Add toaster

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,21 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Tooltip } from "react-tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+import { LogSnagProvider } from "@logsnag/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
   return (
+    // <LogSnagProvider
+    //   project="dropp"
+    //   token={process.env.NEXT_PUBLIC_LOGSNAG_TOKEN!}
+    // >
     <QueryClientProvider client={queryClient}>
       <Tooltip id="tt" className="z-10" />
+      <Toaster richColors />
       <Component {...pageProps} />
     </QueryClientProvider>
+    // </LogSnagProvider>
   );
 }
