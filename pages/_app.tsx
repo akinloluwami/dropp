c03b954541a99e5e7dcaf88764a5e9b9d56f3e26
akinloluwami <akinkunmioye42@gmail.com>
2024-10-01 20:32:58 +0100

    Track with OpenPanel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
 import { LogSnagProvider } from "@logsnag/react";
 import { Analytics } from "@vercel/analytics/react";
+import { OpenPanelComponent } from "@openpanel/nextjs";
+import { useUserStore } from "@/stores/user";
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
+  const { user } = useUserStore();
   return (
     // <LogSnagProvider
     //   project="dropp"
@@ -17,6 +20,13 @@ export default function App({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <Tooltip id="tt" className="z-10" />
       <Toaster richColors />
+      <OpenPanelComponent
+        clientId="a980153d-3bea-4786-b911-34c7b5823a41"
+        trackScreenViews={true}
+        trackAttributes={true}
+        trackOutgoingLinks={true}
+        profileId={user.id}
+      />
       <Analytics />
       <Component {...pageProps} />
     </QueryClientProvider>
