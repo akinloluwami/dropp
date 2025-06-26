61fde29302e6a94768a796ca5e881d9c71fa8c4b
titanium-dev <akinkunmioye42@gmail.com>
2025-06-26 04:23:15 +0100

    Update padding in dashboard layout for improved spacing

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex p-2 gap-x-10">
       <Sidebar />
-      <div className="w-full ml-72 px-16 py-5">{children}</div>
+      <div className="w-full ml-72 px-16 py-3">{children}</div>
     </div>
   );
 };
