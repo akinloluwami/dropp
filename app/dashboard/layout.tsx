41241feea448f8b1561f70cf582fc489cddbd7f8
titanium-dev <akinkunmioye42@gmail.com>
2025-06-26 03:38:08 +0100

    Add DashboardLayout component with sidebar integration for improved layout structure

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Sidebar from "./components/sidebar";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex p-2 gap-x-10">
+      <Sidebar />
+      <div className="w-full ml-72 px-16 py-5">{children}</div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
